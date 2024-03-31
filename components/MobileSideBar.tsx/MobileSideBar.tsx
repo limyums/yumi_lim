65a73a816fb09bb12d8530d7b09774de2bf5937c
limyums <yumimm@naver.com>
2024-03-31 13:24:03 -0600

    add mobile responsive

--- components/MobileSideBar.tsx/MobileSideBar.tsx
+++ components/MobileSideBar.tsx/MobileSideBar.tsx
@@ -14,38 +14,43 @@ const BASE_CALSS = "sidebar";
 export default function MobileSideBar() {
   return (
     <div className={BASE_CALSS}>
-      <Image
-        className=""
-        src="/assets/profile.jpeg"
-        alt="profile"
-        width={150}
-        height={150}
-      />
       <div className={`${BASE_CALSS}_info`}>
-        <div className="mobile_info">
-          <h2> Yumi Lim </h2>
-          <div className="subtitle">Web Developer</div>
-          <div className="social">
-            <a target="_blank" href="https://www.instagram.com/space.caroline/">
-              <Instagram size={20} />
-            </a>
-            <a
-              target="_blank"
-              href="https://www.linkedin.com/in/yumi-lim-173b772ab/"
-            >
-              <Linkedin size={20} />
-            </a>
-          </div>
-          <div className="download">
-            <button type="button">
-              <Download size={20} />
-            </button>
+        <div className={`${BASE_CALSS}_info_left`}>
+          <Image
+            className=""
+            src="/assets/profile.jpeg"
+            alt="profile"
+            width={150}
+            height={150}
+          />
+          <div className="mobile_info">
+            <h2> Yumi Lim </h2>
+            <div className="subtitle">Web Developer</div>
+            <div className="social">
+              <a
+                target="_blank"
+                href="https://www.instagram.com/space.caroline/"
+              >
+                <Instagram size={20} />
+              </a>
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/yumi-lim-173b772ab/"
+              >
+                <Linkedin size={20} />
+              </a>
+            </div>
+            <div className="download">
+              <button type="button">
+                <Download size={20} /> CV
+              </button>
+            </div>
           </div>
         </div>
         <div className="contact">
           <div className="contact_list">
             <div className="phone">
-              <Smartphone size={30} />
+              <Smartphone size={20} />
             </div>
             <div>
               <label>Phone</label>
@@ -54,7 +59,7 @@ export default function MobileSideBar() {
           </div>
           <div className="contact_list">
             <div className="mail">
-              <Mail size={30} />
+              <Mail size={20} />
             </div>
             <div>
               <label>Email</label>
@@ -63,7 +68,7 @@ export default function MobileSideBar() {
           </div>
           <div className="contact_list">
             <div className="pin">
-              <MapPin size={30} />
+              <MapPin size={20} />
             </div>
             <div>
               <label>Location</label>
